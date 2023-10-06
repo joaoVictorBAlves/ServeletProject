c6a2e30f2a50fc4d490c91c2c90a237bf1500945
Seu Novo Nome <seu@email.com>
2023-10-06 14:08:40 -0300

    First servlet that returns a json

---
+++ src/java/controller/auth/LoginServlet.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller.auth;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author João victor B Alves
+ */
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         // Construindo o JSON manualmente
+    String json = "{\"nome\": \"John Doe\", \"email\": \"john.doe@example.com\", \"idade\": 30}";
+
+    // Configurando o tipo de conteúdo da resposta para JSON
+    response.setContentType("application/json");
+
+    // Escrevendo o JSON na resposta
+    response.getWriter().write(json);
+   }
+}
